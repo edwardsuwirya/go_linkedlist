85d6695b655043ccafe86b7ae78dbe3b47cb6a66
Edward Suwirya <edward_suwirya@yahoo.com>
2022-05-15 08:10:46 +0700

    Linked List add operation dan iterate list

--- app.go
+++ app.go
@@ -12,11 +12,50 @@ type mySingleLinkedList struct {
 	head *node
 }
 
+func (s *mySingleLinkedList) addFront(name string) {
+	newNode := &node{
+		data: name,
+	}
+	if s.head == nil {
+		s.head = newNode
+	} else {
+		newNode.next = s.head
+		s.head = newNode
+	}
+	s.size++
+}
+func (s *mySingleLinkedList) addBack(name string) {
+	newNode := &node{
+		data: name,
+	}
+	if s.head == nil {
+		s.head = newNode
+	} else {
+		current := s.head
+		for current.next != nil {
+			current = current.next
+		}
+		current.next = newNode
+	}
+	s.size++
+}
+
+func (s *mySingleLinkedList) iterateList() {
+	for node := s.head; node != nil; node = node.next {
+		fmt.Println(node.data)
+	}
+}
+
 func newLinkedList() *mySingleLinkedList {
 	return &mySingleLinkedList{}
 }
 
 func main() {
 	singleList := newLinkedList()
+	singleList.addFront("Maya")
+	singleList.addFront("Dani")
+	singleList.addFront("Victor")
+	singleList.addBack("Zupri")
+	singleList.iterateList()
 	fmt.Println(singleList.size)
 }
